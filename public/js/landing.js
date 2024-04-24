bc16eb8396b124da96e9e636376636a89932cf4f
lelelol <leandrofortunatojf@gmail.com>
2024-04-24 00:59:42 -0300

    Slideshow dos livros atualizado

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,15 +1,20 @@
-function onload(i){ //cria um vetor com uma ordem aleatoria para as imagens e popula o slideshow
-    id="blo"+i.toString();
+function onload(i,k){ //cria um vetor com uma ordem aleatoria para as imagens e popula o slideshow
+    id1="blo"+i.toString();
+    id2="blo"+k.toString();
     var ar = gerarArrayAleatorio(12);
+
     for(k=0;k<12;k++){
-      document.getElementById(id).innerHTML += '<div class="swiper-slide"><img src="../../../public/assets/capas/capa'+ar[k]+'.jpg"></div>';
-    }    
+      document.getElementById(id1).innerHTML += '<div class="swiper-slide"><img src="../../../public/assets/capas/capa'+ar[k]+'.jpg"></div>';
+      if(k+1<12){
+        document.getElementById(id2).innerHTML += '<div class="swiper-slide"><img src="../../../public/assets/capas/capa'+ar[k+1]+'.jpg"></div>';
+      }
+    }   
+    document.getElementById(id2).innerHTML += '<div class="swiper-slide"><img src="../../../public/assets/capas/capa'+ar[0]+'.jpg"></div>'; 
 }
 
-onload(1);
-onload(2);
-onload(3);
-onload(4);
+onload(1,2);
+onload(3,4);
+
 
 function gerarArrayAleatorio(tamanho) {
   var array = [];
@@ -28,6 +33,9 @@ function gerarArrayAleatorio(tamanho) {
 const swiper1 = new Swiper(".bloco1", {
   direction: "vertical",
   loop: true,
+  speed: 1500,
+  draggable: false,
+  allowTouchMove: false,
   autoplay: {
   delay: 4000,
   reverseDirection: true,
@@ -37,29 +45,41 @@ const swiper1 = new Swiper(".bloco1", {
 const swiper2 = new Swiper(".bloco2", {
   direction: "vertical",
   loop: true,
+  speed: 1500,
+  draggable: false,
+  allowTouchMove: false,
   autoplay: {
   delay: 4000,
-  
+  reverseDirection: true,
   disableOnInteraction: false,
 }
 });
 const swiper3 = new Swiper(".bloco3", {
   direction: "vertical",
   loop: true,
-  autoplay: {
-  delay: 4000,
-  disableOnInteraction: false,
-},
-});
-const swiper4 = new Swiper(".bloco4", {
-  direction: "vertical",
-  loop: true,
+  speed: 1500,
+  draggable: false,
+  allowTouchMove: false,
   autoplay: {
   delay: 4000,
   reverseDirection: true,
   disableOnInteraction: false,
 },
 });
+const swiper4 = new Swiper(".bloco4", {
+  direction: "vertical",
+  loop: true,
+  speed: 1500,
+  draggable: false,
+  allowTouchMove: false,
+  autoplay: {
+  delay: 4000,
+  reverseDirection: true,
+  disableOnInteraction: false,
+},
+});
+
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -69,7 +89,7 @@ function sleep(ms) {
 function mudafoto(){
   var imagem = document.getElementById("mainimg-id");
   imagem.src = "../../../public/assets/arte2.svg";
-  sleep(300).then(() => { imagem.src = "../../../public/assets/arte1.svg"; });
+  sleep(250).then(() => { imagem.src = "../../../public/assets/arte1.svg"; });
 }
 
 
