284085c8b76bcd2ca5e05c30d4fbd23c9529881f
Isaac Soares <isaacsoares.trabalho@gmail.com>
2024-05-10 18:08:40 -0300

    corrige erros de mesclagem de css
    cria funcões abrirModal e fecharModal

---
+++ public/js/admin-lista-de-posts.js
@@ -0,0 +1,7 @@
+function abrirModal(idmodal){
+  document.getElementById(idmodal).style.display = "flex";
+}
+
+function fecharModal(idmodal){
+  document.getElementById(idmodal).style.display = "none";
+}
