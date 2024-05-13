b48fd08616157d1ec282c17f1e38dd00b6092ebd
lelelol <leandrofortunatojf@gmail.com>
2024-05-13 20:56:11 -0300

    modifica imagem quando uma nova é upada

--- public/js/admin-lista-de-posts.js
+++ public/js/admin-lista-de-posts.js
@@ -2,8 +2,8 @@
 document.getElementById('img-edita').addEventListener('change', function (e) {
   var reader = new FileReader();
   reader.onload = function (event) {
-      document.getElementById('preview').src = event.target.result;
-      document.getElementById('preview').style.display = 'block';
+      document.getElementById('preview2').src = event.target.result;
+      document.getElementById('preview2').style.display = 'block';
   }
   reader.readAsDataURL(e.target.files[0]);
 });
@@ -24,8 +24,5 @@ function fecharModal(idmodal){
 }
 
 
-
-
-document.getElementById('data').valueAsDate = new Date();
 document.getElementById('data-adc').valueAsDate = new Date();
 document.getElementById('dataat').valueAsDate = new Date();
