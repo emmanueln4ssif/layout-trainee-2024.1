d6a2b2babc56f41f1f827860016ef6e6126702a0
João Victor Nicácio <joaovictornicaciosilva@gmail.com>
2024-05-13 19:24:30 -0300

    Altera js para importar a imagem

--- public/js/admin-lista-de-posts.js
+++ public/js/admin-lista-de-posts.js
@@ -5,3 +5,19 @@ function abrirModal(idmodal){
 function fecharModal(idmodal){
   document.getElementById(idmodal).style.display = "none";
 }
+
+function verifica(){
+  document.getElementById("img").value = null;
+  document.getElementById("img").onchange = function(){
+      document.getElementById("imgname").innerHTML=document.getElementById("img").value;
+  }
+}
+
+document.getElementById('img').addEventListener('change', function (e) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+      document.getElementById('preview').src = event.target.result;
+      document.getElementById('preview').style.display = 'block';
+  }
+  reader.readAsDataURL(e.target.files[0]);
+});
