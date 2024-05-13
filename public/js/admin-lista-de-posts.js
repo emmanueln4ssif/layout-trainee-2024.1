e0e1a6a9e423d9b546c41cb988bc9895d5793690
lelelol <leandrofortunatojf@gmail.com>
2024-05-13 20:05:18 -0300

    atualiza modal de criação

--- public/js/admin-lista-de-posts.js
+++ public/js/admin-lista-de-posts.js
@@ -1,3 +1,21 @@
+
+document.getElementById('img-edita').addEventListener('change', function (e) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+      document.getElementById('preview').src = event.target.result;
+      document.getElementById('preview').style.display = 'block';
+  }
+  reader.readAsDataURL(e.target.files[0]);
+});
+document.getElementById('img-adc').addEventListener('change', function (e) {
+  var reader = new FileReader();
+  reader.onload = function (event) {
+      document.getElementById('preview').src = event.target.result;
+      document.getElementById('preview').style.display = 'block';
+  }
+  reader.readAsDataURL(e.target.files[0]);
+});
+
 function abrirModal(idmodal){
   document.getElementById(idmodal).style.display = "flex";
 }
@@ -6,18 +24,9 @@ function fecharModal(idmodal){
   document.getElementById(idmodal).style.display = "none";
 }
 
-function verifica(){
-  document.getElementById("img").value = null;
-  document.getElementById("img").onchange = function(){
-      document.getElementById("imgname").innerHTML=document.getElementById("img").value;
-  }
-}
 
-document.getElementById('img').addEventListener('change', function (e) {
-  var reader = new FileReader();
-  reader.onload = function (event) {
-      document.getElementById('preview').src = event.target.result;
-      document.getElementById('preview').style.display = 'block';
-  }
-  reader.readAsDataURL(e.target.files[0]);
-});
+
+
+document.getElementById('data').valueAsDate = new Date();
+document.getElementById('data-adc').valueAsDate = new Date();
+document.getElementById('dataat').valueAsDate = new Date();
