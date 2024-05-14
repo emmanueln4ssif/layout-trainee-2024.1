792b6fe6c0a4786723cb35569b26a366eea8f725
lelelol <leandrofortunatojf@gmail.com>
2024-05-13 21:15:26 -0300

    ajusta visibilidade da senha

--- public/js/lista-usuarios.js
+++ public/js/lista-usuarios.js
@@ -6,10 +6,12 @@ function fechabotao (id){
     document.getElementById(id).style.display = "none";
 }
 
-var senha = document.getElementById('senha')
-var botao = document.getElementById('icone-olho')
+
+
 
 function mostrarSenha(){
+    var senha = document.getElementById('senha')
+    var botao = document.getElementById('icone-olho')
     if(senha.type == 'password'){
         senha.setAttribute('type', 'text')
         botao.setAttribute('src', '/public/assets/olho-fechado.svg')
@@ -17,5 +19,17 @@ function mostrarSenha(){
     else{
         senha.setAttribute('type', 'password')
         botao.setAttribute('src', '/public/assets/olho-aberto.svg')
+    }
+}
+function mostrarSenhaAdc(){
+    var senha1 = document.getElementById('senha1')
+    var botao1 = document.getElementById('icone-olho1')
+    if(senha1.type == 'password'){
+        senha1.setAttribute('type', 'text')
+        botao1.setAttribute('src', '/public/assets/olho-fechado.svg')
+    }
+    else{
+        senha1.setAttribute('type', 'password')
+        botao1.setAttribute('src', '/public/assets/olho-aberto.svg')
     }
 }
