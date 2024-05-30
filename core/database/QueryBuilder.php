8b71d74360c06060489dce2482e4606162a6ff61
lelelol <leandrofortunatojf@gmail.com>
2024-05-30 14:52:11 -0300

    coloca comentário na query

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -30,6 +30,7 @@ class QueryBuilder
     }
     public function selectAllcomNome($table)
     {
+        //seleciona tudo da $table e name da tabela users quando o user_id da $table é igual ao id da tabela user
         $sql = sprintf('SELECT %s.*, users.name FROM %s INNER JOIN users ON %s.user_id = users.id', $table,$table,$table);
 
         try {
