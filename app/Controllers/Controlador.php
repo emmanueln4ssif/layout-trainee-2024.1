3a1d1f7332171881e2789ad9a4d767a930223f71
lelelol <leandrofortunatojf@gmail.com>
2024-05-30 14:49:59 -0300

    retira var dump pq deu tudo certo =)

--- app/Controllers/Controlador.php
+++ app/Controllers/Controlador.php
@@ -11,8 +11,6 @@ class Controlador
     public function tabelaPosts()
     {
         $posts = App::get('database')->selectAllcomNome('posts');
-        var_dump($posts);
-
         return view('admin/admin-lista-de-posts',$posts);
     }
 }
