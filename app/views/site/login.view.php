9ab76ab209da732d1a5f94bff6b723e2710c83ef
lelelol <leandrofortunatojf@gmail.com>
2024-06-25 10:45:59 -0300

    arruma icone do login

--- app/views/site/login.view.php
+++ app/views/site/login.view.php
@@ -22,8 +22,7 @@
         <div class="direita">
             <div class="cabecalho">
                 <img id="marcador-de-livro" src="/public/assets/marcador-de-livro.svg" alt="Marcador de livro">
-                <img id="logo-imagem" src="/public/assets/logo-direita.svg" alt="Logo Story Stroll"
-                    onclick="window.location.href='/'">
+                <img id="logo-imagem" src="/public/assets/logo-direita.svg" alt="Logo Story Stroll" onclick="window.location.href='/'">
             </div>
 
             <p id="titulo-login">Bem vindo de volta!</p>
@@ -33,13 +32,11 @@
                     <div class="campos">
                         <div class="senha-campo">
                             <div class="circulo" style="position:unset;">
-                                <img id="icone-senha" src="/public/assets/icone-senha.svg"
-                                    alt="Ícone de cadeado representando senha">
+                                <img id="icone-senha" src="/public/assets/icone-e-mail.svg" alt="Ícone de cadeado representando senha">
                             </div>
 
                             <label for="email"></label>
-                            <input id="email" oninput="apagaMsg()" type="email" name="email"
-                                placeholder="Digite seu e-mail" required>
+                            <input id="email" oninput="apagaMsg()" type="email" name="email" placeholder="Digite seu e-mail" required>
 
 
                         </div>
@@ -47,26 +44,24 @@
                     <div class="campos">
                         <div class="senha-campo">
                             <div class="circulo" style="position:unset;">
-                                <img id="icone-senha" src="/public/assets/icone-senha.svg"
-                                    alt="Ícone de cadeado representando senha">
+                                <img id="icone-senha" src="/public/assets/icone-senha.svg" alt="Ícone de cadeado representando senha">
                             </div>
 
-                            <input id="senha" oninput="apagaMsg()" type="password" name="senha"
-                                placeholder="Digite sua senha" required>
+                            <input id="senha" oninput="apagaMsg()" type="password" name="senha" placeholder="Digite sua senha" required>
                             <div onclick="mostrarSenha()">
-                                <img id="icone-olho" src="/public/assets/olho-aberto.svg"
-                                    alt="Ícone de olho representando a visibilidade da senha">
+                                <img id="icone-olho" src="/public/assets/olho-aberto.svg" alt="Ícone de olho representando a visibilidade da senha">
                             </div>
                         </div>
                     </div>
                     <p id="valida"><?php
-                        function erro(){
-                          if($_GET['s']==1){
-                            echo'Email ou senha inválidos!';
-                          }
-                        }
-                        @erro();
-                        ?></p>
+                                    function erro()
+                                    {
+                                        if ($_GET['s'] == 1) {
+                                            echo 'Email ou senha inválidos!';
+                                        }
+                                    }
+                                    @erro();
+                                    ?></p>
                     <input id="btn-login" type="submit" value="Logar"></input>
                 </form>
             </div>
