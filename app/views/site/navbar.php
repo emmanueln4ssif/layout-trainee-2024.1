dc2a2d9a6faa20c0cd74cb53edf737b13ad8d2eb
lelelol <leandrofortunatojf@gmail.com>
2024-06-24 22:15:46 -0300

    trocar login por dashboard qnd logado

--- app/views/site/navbar.php
+++ app/views/site/navbar.php
@@ -1,3 +1,8 @@
+<?php
+
+session_start();
+
+?>
 <link rel="stylesheet" href="/public/css/NB-e-footer.css">
 
 <header>
@@ -7,7 +12,14 @@
                 <img id="nav-logo" onclick="location.href='/'" src="/public/assets/LOGO_BRANCA-removebg V1.png"
                     alt="Story Stroll" style="cursor: pointer;">
                 <ul>
-                    <li><a href="/dashboard">Login</a></li>
+                    <?php 
+                    if(!isset ($_SESSION['login']) == true){
+                       echo '<li><a href="/dashboard">Login</a></li>';
+                    }else{
+                        echo '<li><a href="/dashboard">Dashboard</a></li>';
+                    }
+                    ?>
+
                     <li><a href="/publicacoes">Publicações</a></li>
                     <li><a href="/">Home</a></li>
                 </ul>
